727294b3ec8ec44ec4296534fd4001c734ee4df3
Chris Norman <chris.norman2@googlemail.com>
2021-12-16 12:47:27 +0000

    Start using constants.

---
+++ lib/src/constants.dart
@@ -0,0 +1,8 @@
+/// Provides constants for use in the program.
+library constants;
+
+/// The base URL for HTTP requests.
+const baseUrl = 'https://www.3r.org.uk';
+
+/// How often should HTTP requests be repeated.
+const httpGetInterval = Duration(minutes: 10);
