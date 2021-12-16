75e239e26095a29fdcd915621e2524b2fa726931
Chris Norman <chris.norman2@googlemail.com>
2021-12-16 15:10:53 +0000

    Clean up home page code.

--- lib/src/widgets/home_page.dart
+++ lib/src/widgets/home_page.dart
@@ -67,177 +67,7 @@ class _HomePageState extends State<HomePage> {
           if (snapshot.hasData) {
             final preferences =
                 Preferences.fromSharedPreferences(snapshot.requireData);
-            final Widget child;
-            final String title;
-            final apiKey = preferences.apiKey;
-            if (apiKey == null) {
-              title = 'Error';
-              child = const Center(
-                child: Text('You must first add your API key.'),
-              );
-            } else {
-              final options = BaseOptions(headers: getHeaders(apiKey: apiKey));
-              final http = Dio(options);
-              switch (_states) {
-                case HomePageStates.shifts:
-                  title = 'Shifts';
-                  final shiftList = _shiftList;
-                  final shiftsDownloaded = _shiftsDownloaded;
-                  final today = DateTime.now();
-                  if (shiftList == null ||
-                      shiftsDownloaded == null ||
-                      today.difference(shiftsDownloaded) >= httpGetInterval) {
-                    final yesterday = today.subtract(const Duration(days: 1));
-                    final tomorrow = today.add(const Duration(days: 1));
-                    var stuff = [
-                      yesterday.year,
-                      padNumber(yesterday.month),
-                      padNumber(yesterday.day)
-                    ];
-                    final startDate = stuff.join('-');
-                    stuff = [
-                      tomorrow.year,
-                      padNumber(tomorrow.month),
-                      padNumber(tomorrow.day)
-                    ];
-                    final endDate = stuff.join('-');
-                    final future = http.get<JsonType>(
-                      '$baseUrl/shift.json',
-                      queryParameters: <String, dynamic>{
-                        'start_date': startDate,
-                        'end_date': endDate
-                      },
-                    );
-                    child = GetUrlWidget(
-                      future: future,
-                      onLoad: (json) {
-                        if (json == null) {
-                          return const Center(
-                              child: Text('No shifts to show.'));
-                        }
-                        final shiftList = ShiftList.fromJson(json);
-                        _shiftList = shiftList;
-                        _shiftsDownloaded = today;
-                        return ShiftsView(
-                          shifts: getShifts(shiftList),
-                          apiKey: apiKey,
-                        );
-                      },
-                    );
-                  } else {
-                    child = ShiftsView(
-                        shifts: getShifts(shiftList), apiKey: apiKey);
-                  }
-                  break;
-                case HomePageStates.volunteers:
-                  title = 'Volunteers';
-                  final volunteers = _volunteerList?.volunteers;
-                  final volunteersDownloaded = _volunteersDownloaded;
-                  final now = DateTime.now();
-                  if (volunteers == null ||
-                      volunteersDownloaded == null ||
-                      now.difference(volunteersDownloaded) >= httpGetInterval) {
-                    final future =
-                        http.get<JsonType>('$baseUrl/directory.json');
-                    child = GetUrlWidget(
-                      future: future,
-                      onLoad: (json) {
-                        if (json == null) {
-                          return const Center(
-                            child: Text('No volunteers to show.'),
-                          );
-                        }
-                        _volunteersDownloaded = now;
-                        final volunteerList = VolunteerList.fromJson(json);
-                        _volunteerList = volunteerList;
-                        return VolunteersView(
-                          volunteers: volunteerList.volunteers ?? [],
-                          apiKey: apiKey,
-                        );
-                      },
-                    );
-                  } else {
-                    child =
-                        VolunteersView(volunteers: volunteers, apiKey: apiKey);
-                  }
-                  break;
-                case HomePageStates.news:
-                  title = 'News';
-                  child = const Focus(child: Text('News'));
-                  break;
-              }
-            }
-            final tabCallback = CallbackAction(
-              onInvoke: (intent) {
-                if (intent is ShiftsTabIntent) {
-                  setState(() {
-                    _states = HomePageStates.shifts;
-                  });
-                } else if (intent is VolunteersTabIntent) {
-                  setState(() {
-                    _states = HomePageStates.volunteers;
-                  });
-                } else if (intent is NewsTabIntent) {
-                  setState(() {
-                    _states = HomePageStates.news;
-                  });
-                } else {
-                  throw UnimplementedError('Unsupported intent: $intent.');
-                }
-              },
-            );
-            return Shortcuts(
-              shortcuts: const {
-                SingleActivator(LogicalKeyboardKey.digit1, control: true):
-                    ShiftsTabIntent(),
-                SingleActivator(LogicalKeyboardKey.digit2, control: true):
-                    VolunteersTabIntent(),
-                SingleActivator(LogicalKeyboardKey.digit3, control: true):
-                    NewsTabIntent()
-              },
-              child: Actions(
-                actions: {
-                  ShiftsTabIntent: tabCallback,
-                  VolunteersTabIntent: tabCallback,
-                  NewsTabIntent: tabCallback
-                },
-                child: Scaffold(
-                  appBar: AppBar(
-                    title: Text(title),
-                    leading: ElevatedButton(
-                      onPressed: () => Navigator.of(context)
-                          .pushReplacementNamed(ApiKeyForm.routeName,
-                              arguments: preferences),
-                      child: Icon(
-                        Icons.settings,
-                        semanticLabel:
-                            '${apiKey == null ? "Enter" : "Change"} API key',
-                      ),
-                    ),
-                  ),
-                  body: child,
-                  bottomNavigationBar: BottomNavigationBar(
-                    items: const [
-                      BottomNavigationBarItem(
-                          icon: Icon(Icons.calendar_today_rounded),
-                          label: 'Shifts'),
-                      BottomNavigationBarItem(
-                          icon: Icon(Icons.people_rounded),
-                          label: 'Volunteers'),
-                      BottomNavigationBarItem(
-                          icon: Icon(Icons.info_rounded), label: 'News')
-                    ],
-                    currentIndex: _states.index,
-                    onTap: preferences.apiKey == null
-                        ? null
-                        : (value) => setState(() {
-                              _states = HomePageStates.values.firstWhere(
-                                  (element) => element.index == value);
-                            }),
-                  ),
-                ),
-              ),
-            );
+            return getHomePage(preferences);
           } else {
             return Scaffold(
               appBar: AppBar(
@@ -253,26 +83,31 @@ class _HomePageState extends State<HomePage> {
       );
 
   /// Get a list of relevant shifts.
-  List<Shift> getShifts(ShiftList shiftList) {
-    final possibleShifts =
-        shiftList.shifts.where((element) => element.allDay == false).toList()
-          ..sort((a, b) {
-            final result = a.start.compareTo(b.start);
-            if (result == 0) {
-              if (a.rota.name.startsWith('Leader')) {
-                return -1;
-              }
-              return b.rota.name.compareTo(a.rota.name);
-            }
-            return result;
-          });
+  List<Shift> getShifts(
+      {required ShiftList shiftList, required Preferences preferences}) {
+    final possibleShifts = shiftList.shifts
+        .where((element) =>
+            element.allDay == false &&
+            preferences.ignoredShiftIds.contains(element.id) == false)
+        .toList()
+      ..sort((a, b) {
+        final result = a.start.compareTo(b.start);
+        if (result == 0) {
+          if (a.rota.name.startsWith('Leader')) {
+            return -1;
+          }
+          return b.rota.name.compareTo(a.rota.name);
+        }
+        return result;
+      });
     final now = DateTime.now();
     final shifts = shiftList.shifts
         .where((element) =>
             element.allDay == true &&
             element.start.year == now.year &&
             element.start.month == now.month &&
-            element.start.day == now.day)
+            element.start.day == now.day &&
+            preferences.ignoredShiftIds.contains(element.id) == false)
         .toList();
     DateTime? previousStartTime;
     DateTime? nextStartTime;
@@ -299,5 +134,177 @@ class _HomePageState extends State<HomePage> {
           .where((element) => element.start.isAtSameMomentAs(nextStartTime!)));
     }
     return shifts;
+  }
+
+  /// Get the main home page.
+  Widget getHomePage(Preferences preferences) {
+    final Widget child;
+    final String title;
+    final apiKey = preferences.apiKey;
+    if (apiKey == null) {
+      title = 'API Key Required';
+      child = const Center(
+        child: Card(child: Text('You must first add your API key.')),
+      );
+    } else {
+      final options = BaseOptions(headers: getHeaders(apiKey: apiKey));
+      final http = Dio(options);
+      switch (_states) {
+        case HomePageStates.shifts:
+          title = 'Shifts';
+          final shiftList = _shiftList;
+          final shiftsDownloaded = _shiftsDownloaded;
+          final today = DateTime.now();
+          if (shiftList == null ||
+              shiftsDownloaded == null ||
+              today.difference(shiftsDownloaded) >= httpGetInterval) {
+            final yesterday = today.subtract(const Duration(days: 1));
+            final tomorrow = today.add(const Duration(days: 1));
+            var stuff = [
+              yesterday.year,
+              padNumber(yesterday.month),
+              padNumber(yesterday.day)
+            ];
+            final startDate = stuff.join('-');
+            stuff = [
+              tomorrow.year,
+              padNumber(tomorrow.month),
+              padNumber(tomorrow.day)
+            ];
+            final endDate = stuff.join('-');
+            final future = http.get<JsonType>(
+              '$baseUrl/shift.json',
+              queryParameters: <String, dynamic>{
+                'start_date': startDate,
+                'end_date': endDate
+              },
+            );
+            child = GetUrlWidget(
+              future: future,
+              onLoad: (json) {
+                if (json == null) {
+                  return const Center(child: Text('No shifts to show.'));
+                }
+                final shiftList = ShiftList.fromJson(json);
+                _shiftList = shiftList;
+                _shiftsDownloaded = today;
+                return ShiftsView(
+                  shifts:
+                      getShifts(preferences: preferences, shiftList: shiftList),
+                  apiKey: apiKey,
+                );
+              },
+            );
+          } else {
+            child = ShiftsView(
+                shifts:
+                    getShifts(preferences: preferences, shiftList: shiftList),
+                apiKey: apiKey);
+          }
+          break;
+        case HomePageStates.volunteers:
+          title = 'Volunteers';
+          final volunteers = _volunteerList?.volunteers;
+          final volunteersDownloaded = _volunteersDownloaded;
+          final now = DateTime.now();
+          if (volunteers == null ||
+              volunteersDownloaded == null ||
+              now.difference(volunteersDownloaded) >= httpGetInterval) {
+            final future = http.get<JsonType>('$baseUrl/directory.json');
+            child = GetUrlWidget(
+              future: future,
+              onLoad: (json) {
+                if (json == null) {
+                  return const Center(
+                    child: Text('No volunteers to show.'),
+                  );
+                }
+                _volunteersDownloaded = now;
+                final volunteerList = VolunteerList.fromJson(json);
+                _volunteerList = volunteerList;
+                return VolunteersView(
+                  volunteers: volunteerList.volunteers ?? [],
+                  apiKey: apiKey,
+                );
+              },
+            );
+          } else {
+            child = VolunteersView(volunteers: volunteers, apiKey: apiKey);
+          }
+          break;
+        case HomePageStates.news:
+          title = 'News';
+          child = const Focus(child: Text('News'));
+          break;
+      }
+    }
+    final tabCallback = CallbackAction(
+      onInvoke: (intent) {
+        if (intent is ShiftsTabIntent) {
+          setState(() {
+            _states = HomePageStates.shifts;
+          });
+        } else if (intent is VolunteersTabIntent) {
+          setState(() {
+            _states = HomePageStates.volunteers;
+          });
+        } else if (intent is NewsTabIntent) {
+          setState(() {
+            _states = HomePageStates.news;
+          });
+        } else {
+          throw UnimplementedError('Unsupported intent: $intent.');
+        }
+      },
+    );
+    return Shortcuts(
+      shortcuts: const {
+        SingleActivator(LogicalKeyboardKey.digit1, control: true):
+            ShiftsTabIntent(),
+        SingleActivator(LogicalKeyboardKey.digit2, control: true):
+            VolunteersTabIntent(),
+        SingleActivator(LogicalKeyboardKey.digit3, control: true):
+            NewsTabIntent()
+      },
+      child: Actions(
+        actions: {
+          ShiftsTabIntent: tabCallback,
+          VolunteersTabIntent: tabCallback,
+          NewsTabIntent: tabCallback
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(title),
+            leading: ElevatedButton(
+              onPressed: () => Navigator.of(context).pushReplacementNamed(
+                  ApiKeyForm.routeName,
+                  arguments: preferences),
+              child: Icon(
+                Icons.settings,
+                semanticLabel: '${apiKey == null ? "Enter" : "Change"} API key',
+              ),
+            ),
+          ),
+          body: child,
+          bottomNavigationBar: BottomNavigationBar(
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.calendar_today_rounded), label: 'Shifts'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.people_rounded), label: 'Volunteers'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.info_rounded), label: 'News')
+            ],
+            currentIndex: _states.index,
+            onTap: preferences.apiKey == null
+                ? null
+                : (value) => setState(() {
+                      _states = HomePageStates.values
+                          .firstWhere((element) => element.index == value);
+                    }),
+          ),
+        ),
+      ),
+    );
   }
 }
