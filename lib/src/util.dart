13b88ca2f2c03e0e47c1c2ffc61c2804febbf866
Chris Norman <chris.norman2@googlemail.com>
2022-01-24 19:43:41 +0000

    Remove unused functions.

--- lib/src/util.dart
+++ lib/src/util.dart
@@ -32,7 +32,5 @@ String prettyDate(DateTime dateTime) {
 /// Return a number padded with 0's.
 String padNumber(int n) => n.toString().padLeft(2, '0');
 
-/// Get the URL to the image of a volunteer with the given [id].
-
 /// The URL to the image of this volunteer.
 String getImageUrl(int id) => '$baseUrl/directory/$id/photos/thumb.jpg';
