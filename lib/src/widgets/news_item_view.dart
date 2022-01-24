03c1065da75a0e1a798c9f0ca259cb92702eeae8
Chris Norman <chris.norman2@googlemail.com>
2022-01-24 15:23:03 +0000

    Use the html_flutter package to render news items (still broken).

--- lib/src/widgets/news_item_view.dart
+++ lib/src/widgets/news_item_view.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:html_unescape/html_unescape.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-import '../constants.dart';
 import '../json/news_item.dart';
 import '../util.dart';
 import 'cancellable_widget.dart';
@@ -29,101 +27,52 @@ class NewsItemView extends StatefulWidget {
 class _NewsItemViewState extends State<NewsItemView> {
   /// Build a widget.
   @override
-  Widget build(BuildContext context) {
-    final children = <InlineSpan>[
-      TextSpan(
-          text: '\nCreated: ${prettyDate(widget.newsItem.createdAt)} by '
-              '${widget.newsItem.creator.name}.\n',
-          style: normalStyle),
-    ];
-    final htmlUnescape = HtmlUnescape();
-    for (var line in widget.newsItem.body.split('\n')) {
-      line = line.replaceAll('<p>', '').replaceAll('</p>', '').trim();
-      if (line.isEmpty) {
-        children.add(const TextSpan(text: '\n', style: normalStyle));
-        continue;
-      }
-      final matches = linkRegExp.allMatches(line).toList();
-      if (matches.isEmpty) {
-        children.addAll(checkImages(text: line, htmlUnescape: htmlUnescape));
-        continue;
-      }
-      var lastEnd = 0;
-      for (final match in matches) {
-        if (match.start > lastEnd) {
-          final chunk = line.substring(lastEnd, match.start);
-          children.addAll(checkImages(text: chunk, htmlUnescape: htmlUnescape));
-        }
-        final url = match.group(1)!;
-        final title = match.group(2)!;
-        children.add(
-          TextSpan(
-            text: title,
-            style: linkStyle,
-            recognizer: TapGestureRecognizer()..onTap = () => launch(url),
-          ),
-        );
-        lastEnd = match.end;
-      }
-      children.add(const TextSpan(text: '\n', style: normalStyle));
-    }
-    return CancellableWidget(
+  Widget build(BuildContext context) => CancellableWidget(
         child: Scaffold(
-            appBar: AppBar(
-              title: Text(widget.newsItem.title),
+          appBar: AppBar(
+            title: Text(widget.newsItem.title),
+          ),
+          body: Focus(
+            autofocus: true,
+            child: SingleChildScrollView(
+              child: Html(
+                data:
+                    '<h3>Priority: ${widget.newsItem.priority}</h3>${widget.newsItem.body}',
+                onAnchorTap: (
+                  url,
+                  context,
+                  attributes,
+                  element,
+                ) =>
+                    url == null ? null : launch(url),
+                customImageRenders: {
+                  networkSourceMatcher(domains: ['3r.org.uk', 'www.3r.org.uk']):
+                      (context, attributes, element) {
+                    final src = attributes['src'] ?? 'about:blank';
+                    final alt = attributes['alt'] ?? 'Unlabelled image';
+                    final headers = getHeaders(apiKey: widget.apiKey);
+                    return Image.network(
+                      src,
+                      errorBuilder: (context, error, stackTrace) {
+                        print('Failed to load $alt ($src).');
+                        print('Headers: $headers');
+                        print(error);
+                        print(stackTrace);
+                        return Icon(
+                          Icons.image_not_supported_rounded,
+                          semanticLabel: alt,
+                        );
+                      },
+                      headers: headers,
+                      height: double.tryParse(attributes['height'] ?? ''),
+                      semanticLabel: alt,
+                      width: double.tryParse(attributes['width'] ?? ''),
+                    );
+                  }
+                },
+              ),
             ),
-            body: Focus(
-                child: SelectableText.rich(TextSpan(children: children)))));
-  }
-
-  /// Check the given [text] for HTML `strong` tags.
-  List<InlineSpan> checkStrong(
-      {required String text, required HtmlUnescape htmlUnescape}) {
-    final matches = strongRegExp.allMatches(text).toList();
-    if (matches.isEmpty) {
-      return [TextSpan(text: htmlUnescape.convert(text), style: normalStyle)];
-    }
-    final spans = <InlineSpan>[];
-    var lastEnd = 0;
-    for (final match in matches) {
-      if (match.start > lastEnd) {
-        spans.add(TextSpan(
-            text: htmlUnescape.convert(text.substring(lastEnd, match.start)),
-            style: normalStyle));
-      }
-      spans.add(TextSpan(
-          text: htmlUnescape.convert(match.group(1)!), style: boldStyle));
-      lastEnd = match.end;
-    }
-    return spans;
-  }
-
-  /// Scan the given [text] for images using [imgRegExp], and return a list of
-  /// spans.
-  List<InlineSpan> checkImages(
-      {required String text, required HtmlUnescape htmlUnescape}) {
-    final matches = imgRegExp.allMatches(text).toList();
-    if (matches.isEmpty) {
-      return checkStrong(text: text, htmlUnescape: htmlUnescape);
-    }
-    final spans = <InlineSpan>[];
-    var lastEnd = 0;
-    for (final match in matches) {
-      if (match.start > lastEnd) {
-        spans.addAll(checkStrong(
-            text: text.substring(lastEnd, match.start),
-            htmlUnescape: htmlUnescape));
-      }
-      final altText = match.group(1)!;
-      final url = match.group(2)!;
-      spans.add(WidgetSpan(
-          child: Image.network(
-        url,
-        headers: getHeaders(apiKey: widget.apiKey),
-        semanticLabel: altText,
-      )));
-      lastEnd = match.end;
-    }
-    return spans;
-  }
+          ),
+        ),
+      );
 }
